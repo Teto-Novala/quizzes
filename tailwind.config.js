d9f5c988298c843788f80a1d97ef4dd42a900bdd
Teto-Novala <tetocoding@gmail.com>
2024-11-25 05:44:36 +0700

    set tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+        secondary: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
